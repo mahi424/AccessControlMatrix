39b2f90460c5e71c2d27cfaecbef37d7b6ddf520
mahi <mahaveerchouhan424@gmail.com>
2018-07-07 19:07:01 +0530

    BasicWorking

--- AccessControlListCreator.py
+++ AccessControlListCreator.py
@@ -13,37 +13,52 @@ with open("out2.txt", "r") as f:
 			"filename":data[8],
 			}
 			groupsOfUser[data[2]]=data[3]
-			# owners.add(data[2])
 			if data[2] not in owners:
 				owners.append(data[2])
 			details.append(d)
-print(details)
+# print(details)
 
-print(value)
-# permissions={}
-# temp={}
-# for user in owners:
-# 	print(user)
-# 	for detail in details:
-# 		# print(detail)
-# 		if detail["owner"]==user:
-# 			temp[detail["filename"]]=detail["permission"][1:4]
-# 			# print("case1")
-# 			# permissions[user]=temp
-# 		elif detail["group"]==groupsOfUser[user]:
-# 			temp[detail["filename"]]=detail["permission"][4:7]
-# 			# print("case2")
-# 			# permissions[user]=temp
-# 		else:
-# 			temp[detail["filename"]]=detail["permission"][7:10]
-# 			# print("case3")
-# 		permissions[user]=temp
-# 		temp.clear()
-# 			
-			
+permissions={}
+p=[]
+temp={}
+listOfItems=[]
+for user in owners:
+	# print(user)
+	for detail in details:
+		# print(detail)
+		if detail["owner"]==user:
+			temp[detail["filename"]]=detail["permission"][1:4]
+			x=detail["permission"][1:4]
+			# print("case1")
+		elif detail["group"]==groupsOfUser[user]:
+			temp[detail["filename"]]=detail["permission"][4:7]
+			# print("case2")
+		else:
+			temp[detail["filename"]]=detail["permission"][7:10]
+			# print("case3")
+		if detail["filename"] not in listOfItems:
+			listOfItems.append(detail["filename"])
+	if temp is not None:
+		permissions[user]=dict(temp)
+		temp.clear()
+
 # print(permissions)
-# for acls in permissions.keys():
-# 	print("Username is "+acls)
-# 	for acl in permissions[acls].keys():
-# 		# print(str(acl)+" "+permissions[acls][acl])
-# 		print("{0:^30} ".format(str(acl))+"{} ".format(permissions[acls][acl]))
+# print(listOfItems)
+items="     "
+underline="====="
+for item in listOfItems:
+	items+="{0:^25}".format(item)
+	underline+="{0:=^25}".format("")
+print(items)
+print(underline)
+
+row=""
+for user in permissions.keys():
+	# print(user)
+	row=""
+	row=row+str(user)
+	# for key in permissions[user].keys():
+	# 	row+="{0:^25}".format(str(permissions[user][key]))
+	for key in listOfItems:
+		row+="{0:^25}".format(str(permissions[user][key]))
+	print(row)
